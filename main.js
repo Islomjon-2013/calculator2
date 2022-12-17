0dd46265df1e2bbc8b105a53166addf7d9ec7adf
hulkar <hulkar@gmail.com>
2022-12-16 19:57:49 -0500

    created useful calc

---
+++ main.js
@@ -0,0 +1,49 @@
+let number1 = document.querySelector('input[name=num1]');
+let number2 = document.querySelector('input[name = num2]');
+let select = document.querySelector('select');
+let res = document.querySelector('.res');
+let btn = document.querySelector(`button`)
+
+ function point(){
+    number1.value="";
+    number2.value="";
+    btn.setAttribute(`disabled`,`disabled`)
+ }
+
+
+function result(){
+    let num1 = +number1.value;
+   let  num2 = +number2.value;
+    res.innerHTML = num1 + num2;
+
+switch(select.value){
+    case 'sum':
+        res.innerHTML = num1+num2;
+        point()
+        break;
+        case 'sub':
+            res.innerHTML = num1-num2;
+            point()
+            break;
+            case 'mul':
+            res.innerHTML = num1*num2;
+            point()
+            break;
+            case 'div':
+            res.innerHTML = num1/num2;
+            point()
+            case 'exp':
+                res.innerHTML=num1**2;
+                point()
+                break;
+          case 'exp2':
+            res.innerHTML= num2**2;
+            point()
+            break;
+            default:
+}
+}
+
+
+
+
